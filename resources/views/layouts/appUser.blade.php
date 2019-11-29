227f42c30c98c8020d25f96b4c7105c97110f917
Matthew Hill <matthill500@hotmail.com>
2019-11-29 10:35:35 +0000

    update navbar

--- resources/views/layouts/appUser.blade.php
+++ resources/views/layouts/appUser.blade.php
@@ -52,9 +52,7 @@
                 <a class="navbar-brand" href="{{ url('/user/questions') }}">
                   My Questions
                 </a>
-                <a class="navbar-brand" href="#">
-                  My Answers
-                </a>
+                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
